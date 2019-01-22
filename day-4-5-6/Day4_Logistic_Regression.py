9105e70f01413a31046840076e8222c100a37500
Yiding He <yhe@truxnow.com>
2019-01-22 16:31:21 -0500

    Added Day4 dataset and code.

---
+++ day-4-5-6/Day4_Logistic_Regression.py
@@ -0,0 +1,68 @@
+# Step1 Data Preprocessing
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('../datasets/Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+Y = dataset.iloc[:,4].values
+print('X')
+print(X)
+print('Y')
+print(Y)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.fit_transform(X_test)
+
+# Step2 Logistic Regression Model
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression()
+classifier.fit(X_train, Y_train)
+
+# Step3 Predection
+y_pred = classifier.predict(X_test)
+
+# Step4 Evaluate the Predection
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test, y_pred)
+
+from matplotlib.colors import ListedColormap
+X_set,Y_set=X_train,Y_train
+X1,X2=np. meshgrid(np. arange(start=X_set[:,0].min()-1, stop=X_set[:, 0].max()+1, step=0.01),
+                   np. arange(start=X_set[:,1].min()-1, stop=X_set[:,1].max()+1, step=0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+for i,j in enumerate(np. unique(Y_set)):
+    plt.scatter(X_set[Y_set==j,0],X_set[Y_set==j,1],
+                c = ListedColormap(('red', 'green'))(i), label=j)
+
+plt. title(' LOGISTIC(Training set)')
+plt. xlabel(' Age')
+plt. ylabel(' Estimated Salary')
+plt. legend()
+plt. show()
+
+X_set,Y_set=X_test,Y_test
+X1,X2=np. meshgrid(np. arange(start=X_set[:,0].min()-1, stop=X_set[:, 0].max()+1, step=0.01),
+                   np. arange(start=X_set[:,1].min()-1, stop=X_set[:,1].max()+1, step=0.01))
+
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+for i,j in enumerate(np. unique(Y_set)):
+    plt.scatter(X_set[Y_set==j,0],X_set[Y_set==j,1],
+                c = ListedColormap(('red', 'green'))(i), label=j)
+
+plt. title(' LOGISTIC(Test set)')
+plt. xlabel(' Age')
+plt. ylabel(' Estimated Salary')
+plt. legend()
+plt. show()
