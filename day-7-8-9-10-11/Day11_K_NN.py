3050a5a5fd137316e22814c36ab122f0f7b5aec3
Yiding He <yhe@truxnow.com>
2019-02-27 10:56:51 -0500

    Updated the code.

--- day-7-8-9-10-11/Day11_K_NN.py
+++ day-7-8-9-10-11/Day11_K_NN.py
@@ -16,4 +16,56 @@ print(Y)
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
 
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 
+# Step2 KNN train the Model
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5, metric="minkowski", p=2)
+classifier.fit(X_train, Y_train)
+
+# Step3 Predection
+Y_pred = classifier.predict(X_test)
+
+# Step4 Evaluate the Prediction
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test, Y_pred)
+
+# Step5 Visualization
+from matplotlib.colors import ListedColormap
+X_set,Y_set=X_train,Y_train
+X1,X2=np. meshgrid(np. arange(start=X_set[:,0].min()-1, stop=X_set[:, 0].max()+1, step=0.01),
+                   np. arange(start=X_set[:,1].min()-1, stop=X_set[:,1].max()+1, step=0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+for i,j in enumerate(np. unique(Y_set)):
+    plt.scatter(X_set[Y_set==j,0],X_set[Y_set==j,1],
+                c = ListedColormap(('red', 'green'))(i), label=j)
+
+plt. title(' KNN(Training set)')
+plt. xlabel(' Age')
+plt. ylabel(' Estimated Salary')
+plt. legend()
+plt. show()
+
+X_set,Y_set=X_test,Y_test
+X1,X2=np. meshgrid(np. arange(start=X_set[:,0].min()-1, stop=X_set[:, 0].max()+1, step=0.01),
+                   np. arange(start=X_set[:,1].min()-1, stop=X_set[:,1].max()+1, step=0.01))
+
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+for i,j in enumerate(np. unique(Y_set)):
+    plt.scatter(X_set[Y_set==j,0],X_set[Y_set==j,1],
+                c = ListedColormap(('red', 'green'))(i), label=j)
+
+plt. title(' KNN(Test set)')
+plt. xlabel(' Age')
+plt. ylabel(' Estimated Salary')
+plt. legend()
+plt. show()
